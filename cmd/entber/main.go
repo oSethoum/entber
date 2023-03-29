fee78c2052a36d5d1f57eda6ba3d50bd714548e9
oSethoum <os.sethoum@gmail.com>
2023-03-29 18:49:12 +0100

    move dev to linux

--- cmd/entber/main.go
+++ cmd/entber/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"io/fs"
+	"log"
+	"os"
+	"path"
 
+	"entgo.io/ent/entc/gen"
+)
+
+//go:embed templates
+var templates embed.FS
+
+func main() {
+	writeTemplae("entc", "entc.go")
+	writeTemplae("generate", "generate.go")
+}
+
+func writeTemplae(name string, p string) {
+	in, err := templates.ReadFile("templates/" + name + ".tmpl")
+	catch(err)
+	t, err := template.New(name).Funcs(gen.Funcs).Parse(string(in))
+	catch(err)
+	out := new(bytes.Buffer)
+	err = t.Execute(out, nil)
+	catch(err)
+	p = "ent/generate/" + p
+	err = os.MkdirAll(path.Dir(p), 06666)
+	catch(err)
+	err = os.WriteFile("ent/generate/"+p, out.Bytes(), fs.ModePerm)
+	catch(err)
+}
+
+func catch(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
