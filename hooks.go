1c8c435074a89be8e906b36e2ea174cd7faa5365
oSethoum <os.sethoum@gmail.com>
2023-06-17 03:52:44 +0100

    update subscription ws

--- hooks.go
+++ hooks.go
@@ -34,8 +34,8 @@ func (e *extension) generate(next gen.Generator) gen.Generator {
 			s = parseTemplate("fiber/handlers/ws", e.data)
 			writeFile(path.Join(destination, "handlers/ws.go"), s)
 
-			s = parseTemplate("fiber/handlers/files", e.data)
-			writeFile(path.Join(destination, "handlers/files.go"), s)
+			s = parseTemplate("fiber/utils/utils", e.data)
+			writeFile(path.Join(destination, "utils/utils.go"), s)
 
 			for _, schema := range g.Schemas {
 				e.data.CurrentSchema = schema
